9f2e6f27050e98dad40343d498415d1a5868936f
Vicente Maroto <vicente.maroto@escuelaestech.es>
2021-01-19 18:45:39 +0100

    Unused Outlet

--- SideMenuMapsApp/WelcomeViewController.swift
+++ SideMenuMapsApp/WelcomeViewController.swift
@@ -7,10 +7,7 @@
 
 import UIKit
 class WelcomeViewController: UIViewController {
-    //Outlets
     @IBOutlet weak var mapBtnItem: UIBarButtonItem!
-    @IBOutlet weak var webBtn: UIButton!
-    
     override func viewDidLoad() {
        
         
