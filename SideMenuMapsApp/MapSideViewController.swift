8c28c1a11d54bac9c155d28c8b360a874ae63ee5
Vicente Maroto <vicente.maroto@escuelaestech.es>
2021-01-19 19:09:39 +0100

    Add markers

--- SideMenuMapsApp/MapSideViewController.swift
+++ SideMenuMapsApp/MapSideViewController.swift
@@ -9,13 +9,44 @@ import UIKit
 import MapKit
 class MapSideViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
-    var location = CLLocation(latitude: 38.092711, longitude: -3.6371597)
+    var positoLocation = CLLocation(latitude: 38.092711, longitude: -3.6371597)
+    var estechLocation = CLLocation(latitude: 38.0941902, longitude: -3.6334425)
     let regionRadius:CLLocationDistance = 1000
     override func viewDidLoad() {
-        centerMapOnLocation(location: location)
+        centerMapOnLocation(location: positoLocation)
+        showArtwork(coordinate: positoLocation.coordinate, name: "El Pósito", subtitle: "El Pósito de Linares", discipline:  "Centro de información turística")
+        showArtwork(coordinate: estechLocation.coordinate, name: "EscuelaEstech", subtitle: "Escuela de tecnologias aplicadas", discipline: "Centro de Estudios")
     }
     func centerMapOnLocation(location:CLLocation) -> Void {
         let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
         mapView.setRegion(coordinateRegion, animated: true)
+    }
+    func showArtwork(coordinate:CLLocationCoordinate2D, name:String!, subtitle:String!,discipline:String) {
+        let artwork = Artwork(title:name , locationName:subtitle , discipline:discipline, coordinate: coordinate)
+        mapView.addAnnotation(artwork)
+    }
+}
+
+extension MapSideViewController:MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? Artwork else {return nil}
+        let identifier = "marker"
+        var view:MKMarkerAnnotationView
+        if let dequeueView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView{
+            dequeueView.annotation = annotation
+            view = dequeueView
+        }else{
+            //Crear un nuevo objeto MKMarkerAnnotationView
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return view
+    }
+    func mapView (_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control:UIControl){
+        let location = view.annotation as! Artwork
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+        location.mapItem().openInMaps(launchOptions: launchOptions)
     }
 }
